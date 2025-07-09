331bbdf0bb02cdda2d4306445dcda4ae91b52c15
Andy Salerno <andysalerno@gmail.com>
2025-07-08 18:15:00 -0700

    fix justfile and runit

--- kindlr/kindle/runit.sh
+++ kindlr/kindle/runit.sh
@@ -10,13 +10,14 @@ killall screenControlHF &> /dev/null
 which curl
 which wget
 
+chmod 777 /mnt/us/documents/kindlr
+
 # sleep forever:
 sleep_count=0
 while true; do
     # eips -i > /mnt/us/documents/eipsout.txt
     # eips -g /mnt/us/documents/image.png -w gc16
     echo 'sleeping... ' $sleep_count
-    chmod 777 /mnt/us/documents/kindlr
     /mnt/us/documents/kindlr
     sleep_count=$((sleep_count + 1))
     sleep 30
