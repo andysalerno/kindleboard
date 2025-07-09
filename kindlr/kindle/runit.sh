1e74fb480e4206a15057bc32322d6ffb73e7e11b
Andy Salerno <andysalerno@gmail.com>
2025-07-08 21:27:15 -0700

    cleaning up script

--- kindlr/kindle/runit.sh
+++ kindlr/kindle/runit.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Name: ShowImage
 # Author: tiny
-# _DontUseFBInk
+# DontUseFBInk
 
 killall screenControl &> /dev/null
 killall screenControlHF &> /dev/null
@@ -11,18 +11,22 @@ which curl
 which wget
 
 # chmod 777 /mnt/us/documents/kindlr
+# eips -i > /mnt/us/documents/eipsout.txt
 
 rm /mnt/us/documents/image.png
+
+URL="http://192.168.1.214:8000/image"
+IMAGE_PATH="/mnt/us/documents/image.png"
+SLEEP_SEC=30
 
 # sleep forever:
 sleep_count=0
 while true; do
-    # eips -i > /mnt/us/documents/eipsout.txt
-    wget http://192.168.1.179:8000/image -O /mnt/us/documents/image.png
-    # eips -g /mnt/us/documents/image.png -w gc16
+    wget "$URL" -O "$IMAGE_PATH"
+    eips -g "$IMAGE_PATH" -w gc16
     # echo 'sleeping... ' $sleep_count
     # /mnt/us/documents/kindlr
     sleep_count=$((sleep_count + 1))
-    sleep 30
+    sleep $SLEEP_SEC
 done
 exit 0
