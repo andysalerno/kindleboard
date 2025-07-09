f2b541a4b14ad7bb736be442f587bd72fe6140d2
Andy Salerno <andysalerno@gmail.com>
2025-07-08 21:03:17 -0700

    remove the existing image

--- kindlr/kindle/runit.sh
+++ kindlr/kindle/runit.sh
@@ -12,12 +12,15 @@ which wget
 
 # chmod 777 /mnt/us/documents/kindlr
 
+rm /mnt/us/documents/image.png
+
 # sleep forever:
 sleep_count=0
 while true; do
     # eips -i > /mnt/us/documents/eipsout.txt
-    eips -g /mnt/us/documents/image.png -w gc16
-    echo 'sleeping... ' $sleep_count
+    wget http://192.168.1.179:8000/image -O /mnt/us/documents/image.png
+    # eips -g /mnt/us/documents/image.png -w gc16
+    # echo 'sleeping... ' $sleep_count
     # /mnt/us/documents/kindlr
     sleep_count=$((sleep_count + 1))
     sleep 30
